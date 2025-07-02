85f58ad5e181a61d65dd5c375538964163375cd7
JL <joaolori@gmail.com>
2025-07-02 08:09:44 +0100

    fgdf

--- test_viton.py
+++ test_viton.py
@@ -369,7 +369,7 @@ def run_single_pair(person_image_path, cloth_image_path, mask_path, output_path,
             uc = None
             start_code = model.q_sample(warp_feat, ts)
             shape = [4, H // 8, W // 8]
-            samples_ddim, _ = sampler.sample(S=100, conditioning=c, batch_size=1, shape=shape, down_block_additional_residuals=down_block_additional_residuals, verbose=False, unconditional_guidance_scale=1, unconditional_conditioning=uc, eta=0.0, x_T=start_code, use_T_repaint=True, **test_model_kwargs)
+            samples_ddim, _ = sampler.sample(S=100, conditioning=c, batch_size=1, shape=shape, down_block_additional_residuals=down_block_additional_residuals, verbose=False, unconditional_guidance_scale=1, unconditional_conditioning=uc, eta=0.0, x_T=start_code, use_T_repaint=True, test_model_kwargs=test_model_kwargs, **test_model_kwargs)
             samples_ddim = 1/ 0.18215 * samples_ddim
             
             # Clear memory after sampling
